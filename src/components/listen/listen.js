adeb75a236ce03ea60be64dbbdf7ffa63f8a0460
cormacbeagan <cormacbeagan@gmail.com>
2021-04-03 12:19:49 +0200

    added acp link for small screens

--- src/components/listen/listen.js
+++ src/components/listen/listen.js
@@ -1,12 +1,17 @@
 import { useState, useRef, useEffect } from 'react';
 import TwoDay from '../../utils/twoday';
+import { useDimensionsSetter } from '../../utils/useDimensionSetter';
 import './listen.css';
+let twoDayUrl;
 
 function Listen({ onAudioLoad }) {
   const [listen, setListen] = useState('LISTEN');
   const [playing, setPlaying] = useState(false);
   const audio = useRef(null);
   const btn = useRef();
+  const [width] = useDimensionsSetter();
+
+  console.log(width);
 
   useEffect(() => {
     const audioSet = audio.current;
@@ -19,6 +24,14 @@ function Listen({ onAudioLoad }) {
       audioSet.removeEventListener('playing', loader);
     };
   }, []);
+
+  useEffect(() => {
+    if (width < 420) {
+      twoDayUrl = 'https://radio2day.ip-streaming.net/radio2dayaacp';
+    } else {
+      twoDayUrl = 'https://radio2day.ip-streaming.net/radio2day';
+    }
+  }, [width]);
 
   const handleKeyup = (e) => {
     if (e.keyCode === 32) {
@@ -38,7 +51,7 @@ function Listen({ onAudioLoad }) {
       audio.current.src = 'about';
       TwoDay.tearDown();
     } else {
-      audio.current.src = 'https://radio2day.ip-streaming.net/radio2day';
+      audio.current.src = twoDayUrl;
       audio.current.volume = 0.3;
     }
     if (!playing) {
