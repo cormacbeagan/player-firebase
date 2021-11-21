e966cb034e78ce8dd944f40fda6fb1323c80f664
cormacbeagan <cormacbeagan@gmail.com>
2021-11-21 13:52:50 +0100

    add schwarzwald logo when playing

--- src/components/image/image.js
+++ src/components/image/image.js
@@ -1,28 +1,33 @@
-import { useEffect, useState } from 'react'
-import './image.css'
+import { useEffect, useState } from "react";
+import "./image.css";
 
-function Image({ data }) {
-    const [imgStyle, setImgStyle] = useState({ display: 'none' })
+function Image({ data, station }) {
+  const [is2Day, setIs2Day] = useState(station?.name === "2Day");
+  const [imgStyle, setImgStyle] = useState({ display: "none" });
 
-    useEffect(() => {
-        if (data.show) {
-            setImgStyle({ display: 'block' })
-        } else {
-            setImgStyle({ display: 'none' })
-        }
-    }, [data.show])
+  useEffect(() => {
+    if (data.show) {
+      setImgStyle({ display: "block" });
+    } else {
+      setImgStyle({ display: "none" });
+    }
+  }, [data.show]);
 
-    return (
-        <div className='image-container'>
-            <img
-                id='load'
-                src={data.image}
-                className='image'
-                style={imgStyle}
-                alt='Album Cover'
-            />
-        </div>
-    )
+  useEffect(() => {
+    setIs2Day(station?.name === "2Day");
+  }, [station]);
+
+  return (
+    <div className={is2Day ? "image-container" : "image-container schwarz"}>
+      <img
+        id="load"
+        src={data.image}
+        className="image"
+        style={imgStyle}
+        alt="Album Cover"
+      />
+    </div>
+  );
 }
 
-export default Image
+export default Image;
