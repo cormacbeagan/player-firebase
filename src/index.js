3380078b442cb35947e1efa8b19168d029a3b320
cormacbeagan <cormacbeagan@gmail.com>
2021-11-21 13:55:33 +0100

    remove sentry

--- src/index.js
+++ src/index.js
@@ -1,25 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import * as Sentry from '@sentry/react';
-import { Integrations } from '@sentry/tracing';
-import App from './App';
-import './index.css';
-
-Sentry.init({
-  dsn:
-    'https://1c2828a03c7b462cb52e1738cb31bd53@o553009.ingest.sentry.io/5679644',
-  integrations: [new Integrations.BrowserTracing()],
-
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
-});
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 
-  document.getElementById('root')
+  document.getElementById("root")
 );
