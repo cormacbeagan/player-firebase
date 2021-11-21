db606a434a601bf4e99eb3f5ddbf9c166282d4c3
cormacbeagan <cormacbeagan@gmail.com>
2021-11-21 13:55:11 +0100

    switch back to single quote

--- src/components/image/image.js
+++ src/components/image/image.js
@@ -1,24 +1,24 @@
-import { useEffect, useState } from "react";
-import "./image.css";
+import { useEffect, useState } from 'react';
+import './image.css';
 
 function Image({ data, station }) {
-  const [is2Day, setIs2Day] = useState(station?.name === "2Day");
-  const [imgStyle, setImgStyle] = useState({ display: "none" });
+  const [is2Day, setIs2Day] = useState(station?.name === '2Day');
+  const [imgStyle, setImgStyle] = useState({ display: 'none' });
 
   useEffect(() => {
     if (data.show) {
-      setImgStyle({ display: "block" });
+      setImgStyle({ display: 'block' });
     } else {
-      setImgStyle({ display: "none" });
+      setImgStyle({ display: 'none' });
     }
   }, [data.show]);
 
   useEffect(() => {
-    setIs2Day(station?.name === "2Day");
+    setIs2Day(station?.name === '2Day');
   }, [station]);
 
   return (
-    <div className={is2Day ? "image-container" : "image-container schwarz"}>
+    <div className={is2Day ? 'image-container' : 'image-container schwarz'}>
       <img
         id="load"
         src={data.image}
